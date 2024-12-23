e72e098bf1ed4989c7100a4dd09c3492cfc9567e
Christophe <christophe.moana.barth@gmail.com>
2024-12-23 13:07:38 +0100

    New Pokedex2 done

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,16 +1,34 @@
-function PokemonCard() {
+function PokemonCard(props) {
 
     return (
         <>
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-        </figure>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
+        
 
         <figcaption>
-            <h2 className="firstname">Bulbasaur</h2>
+            <h2 className={pokemon.name}>bulbasaur</h2>
+            <h2 className={pokemon.name}>mew</h2>
         </figcaption>
+
+        {/* utilisation d'un ternaire pour vérifier si notre premier pokemon a une photo */}
+        {PokemonCard.imgSrc ? <img src= {pokemon.name}/> : <p>???</p>}
+
+        </figure>
         </>
     )
 }
+
+const pokemon = { name: "mew", imgSrc: null };
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  }
+];
 
 export default PokemonCard;
