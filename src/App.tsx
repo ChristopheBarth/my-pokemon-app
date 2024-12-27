9f8d3e9918e7b09ce07156b78a31b415c8354dc4
Christophe <christophe.moana.barth@gmail.com>
2024-12-27 15:43:29 +0100

    Pokedex4 finally done

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
-function App() {
-  {/* Ici on déplacé le tableau pokemonList du composant PokemonCard vers App */}
 
-  // const pokemon = { name: "mew", imgSrc: null };
-  // const pokemonList = [
-  //   { name: "bulabasaur", imgSrc: PokemonCard},
-  //   { name: "mew", imgSrc: null},
-  // ]
-  const pokemon = { name: "mew", imgSrc: null };
+  {/* Ici on déplacé le tableau pokemonList du composant PokemonCard vers App.tsx */}
+  function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+  
+    }
+  ]
+  
 
   return (
     <div>
       {/* Ici on a crée une prop appelée pokemon et on lui attribue un Pokémon du tableau pokemonList */}
-      <PokemonCard pokemon={pokemon.name} />
-      {/* <PokemonCard pokemon={pokemonList[0]} /> */}
+      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[1]} />
+
     </div>
   );
 }
 
-export default App;
+
+  export default App; 
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,40 +1,30 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-
-
-function PokemonCard ( pokemon ) {
+function PokemonCard({ pokemon }) {
   {/* ajoute un paramètre props à la fonction PokemonCard */}
  
-  
-  {/* const pokemon= pokemonList[1] ici on a remplacé mon tableau pokemonList pour ne pas casser l'affichage par */} 
-  {/* const pokemon = { name: "mew", imgSrc: null }; on a supprimé cette ligne */}
+
   return (
     <>
       <figure>
-           {/* utilisation ici d'un ternaire pour vérifier si Mew à une image */}
-           {pokemon.imgSrc ? <img src= {pokemon.imgSrc}></img> : <p>???</p>}
-           <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-            
-        <figcaption>
+          
+         
+          {/* <img src={pokemon.imgSrc} alt={pokemon.name}></img> */}
+          <img src={pokemon.imgSrc}></img>
 
-          {/* <h2>{pokemon.name}</h2>  */}
-          <h2 className={pokemon.name}>bulbasaur</h2>
-          <h2 className={pokemon.name}>mew</h2>
-        
+        <figcaption>
+ 
+          <h2>{pokemon.name}</h2>
+                
         </figcaption>
 
+        {/* utilisation ici d'un ternaire pour vérifier si Mew à une image */}
+        {pokemon.imgSrc ? <img src= {pokemon.name}></img> : <p>???</p>}
+        
       </figure>
     </>
   )
 }
 
 export default PokemonCard;
+
+
+
